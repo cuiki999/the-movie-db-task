5ad3c808d7ad2c530f3477400f2e4f493fc29916
cuiki999 <lea.opacic@gmail.com>
2025-10-02 12:27:59 +0200

    Propose basic component and scss structure, write sample files

---
+++ src/components/base/BaseSelectableChip.vue
@@ -0,0 +1,44 @@
+<template>
+    <button :class="chipClasses">
+        {{ title }}
+    </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+    isSelected: boolean;
+    title: string;
+    value: number;
+}>();
+
+const chipClasses = computed(() => {
+    const classes = ['chip'];
+
+    if (props.isSelected) {
+        classes.push('selected');
+    }
+
+    return classes.join();
+});
+</script>
+
+<style scoped lang="scss">
+@use '@/assets/scss/abstracts';
+
+.chip {
+    padding: 4px 12px; // todo
+    background: abstracts.$color-background-primary;
+    color: abstracts.$color-text-primary;
+    font-size: 0.9rem;
+    border: 1px solid abstracts.$color-border-dark;
+    border-radius: abstracts.$border-radius-lg;
+
+    &.selected,
+    &:hover {
+        background: abstracts.$color-action;
+        color: abstracts.$color-text-lighten;
+    }
+}
+</style>
